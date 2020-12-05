e765a8e5fc6f90cb44d256978888fe02d1591478
RathipriyaSingaram <72462005+rathipry@users.noreply.github.com>
2020-12-05 16:45:07 +0530

    Add test case for Rental class

--- src/test/java/refactoring/RentalTest.java
+++ src/test/java/refactoring/RentalTest.java
@@ -1,5 +1,28 @@
 package refactoring;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class RentalTest {
 
+    private static final String title = "MP3";
+    private static int priceCode = 2;
+    private static Movie movie ;
+    private static Rental rental;
+    private static final int daysRented = 2;
+
+    @BeforeAll
+    public static void setup() {
+        movie = new Movie(title,priceCode);
+        rental = new Rental(movie, daysRented);
+    }
+
+    @Test
+    public void shouldReturnMovieObjectSetInConstructor() {
+        assertTrue(movie.equals(rental.getMovie()));
+        assertTrue(daysRented == rental.getDaysRented());
+    }
 }
